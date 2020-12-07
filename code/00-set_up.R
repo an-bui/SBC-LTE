a5e2c793a3be3722c83a87299823b0b674a956b3
an-bui <a.thanh.bui@gmail.com>
2020-12-07 13:42:15 -0800

    set up and univariate stuff

--- code/00-set_up.R
+++ code/00-set_up.R
@@ -1,6 +1,11 @@
 # libraries
 library(tidyverse)
 library(here)
+library(janitor)
 
 # data
-biomass <- read_csv(here::here("data", "LTE_All_Species_Biomass_at_transect_20200605.csv"))
+biomass <- read_csv(here::here("data", "LTE_All_Species_Biomass_at_transect_20200605.csv")) %>% 
+  clean_names() %>% 
+  mutate(scientific_name = str_replace(scientific_name, " ", "_"), 
+         treatment = str_replace(treatment, " ", "_")) %>% 
+  unite("sample_ID", site, treatment, year, remove = FALSE) 
