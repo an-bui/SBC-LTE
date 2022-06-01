642e33d45ea5bab17443fb8059ad10fed9e49618
an-bui <a.thanh.bui@gmail.com>
2022-06-01 10:14:52 -0700

    cleaned up set up script

--- code/00-set_up.R
+++ code/00-set_up.R
@@ -38,12 +38,11 @@ library(gtsummary)
 
 # 2. start and end dates --------------------------------------------------
 
+# ⟞ a. Arroyo Quemado (AQUE) ---------------------------------------------
 
-############################################
-# a. Arroyo Quemado (AQUE)
-############################################
-
-aque_start_dates <- c("AQUE_CONTROL_2008-01-30", "AQUE_ANNUAL_2008-01-30", "AQUE_CONTINUAL_2010-04-26")
+aque_start_dates <- c("AQUE_CONTROL_2008-01-30", 
+                      "AQUE_ANNUAL_2008-01-30", 
+                      "AQUE_CONTINUAL_2010-04-26")
 
 aque_start_date <- as_date("2008-01-30")
 
@@ -53,11 +52,12 @@ aque_after_date_annual <- as_date("2018-05-10")
 
 aque_after_date_continual <- as_date("2017-08-16")
 
-############################################
-# b. Naples (NAPL) 
-############################################
 
-napl_start_dates <- c("NAPL_CONTROL_2008-01-10", "NAPL_ANNUAL_2008-01-10", "NAPL_CONTINUAL_2010-04-27")
+# ⟞ b. Naples (NAPL) -----------------------------------------------------
+
+napl_start_dates <- c("NAPL_CONTROL_2008-01-10", 
+                      "NAPL_ANNUAL_2008-01-10", 
+                      "NAPL_CONTINUAL_2010-04-27")
 
 napl_start_date <- as_date("2008-01-10")
 
@@ -67,11 +67,11 @@ napl_after_date_annual <- as_date("2017-05-16")
 
 napl_after_date_continual <- as_date("2016-08-14")
 
-############################################
-# c. Isla Vista (IVEE)
-############################################
 
-ivee_start_dates <- c("IVEE_CONTROL_2011-10-26", "IVEE_ANNUAL_2011-10-26")
+# ⟞ c. Isla Vista (IVEE) --------------------------------------------------
+
+ivee_start_dates <- c("IVEE_CONTROL_2011-10-26", 
+                      "IVEE_ANNUAL_2011-10-26")
 
 ivee_start_date <- as_date("2011-10-26")
 
@@ -79,11 +79,11 @@ ivee_after_date <- as_date("2016-02-18")
 
 ivee_after_date_annual <- as_date("2017-05-15")
 
-############################################
-# d. Mohawk (MOHK)
-############################################
+# ⟞ d. Mohawk (MOHK) ------------------------------------------------------
 
-mohk_start_dates <- c("MOHK_ANNUAL_2008-01-17", "MOHK_CONTROL_2008-01-17", "MOHK_CONTINUAL_2010-05-05")
+mohk_start_dates <- c("MOHK_ANNUAL_2008-01-17", 
+                      "MOHK_CONTROL_2008-01-17", 
+                      "MOHK_CONTINUAL_2010-05-05")
 
 mohk_start_date <- as_date("2008-01-17")
 
@@ -93,11 +93,11 @@ mohk_after_date_annual <- as_date("2018-05-15")
 
 mohk_after_date_continual <- as_date("2017-08-11")
 
-############################################
-# e. Carpinteria (CARP)
-############################################
+# ⟞ e. Carpinteria (CARP)  ------------------------------------------------
 
-carp_start_dates <- c("CARP_CONTROL_2008-02-12", "CARP_ANNUAL_2008-02-12", "CARP_CONTINUAL_2010-04-23")
+carp_start_dates <- c("CARP_CONTROL_2008-02-12", 
+                      "CARP_ANNUAL_2008-02-12", 
+                      "CARP_CONTINUAL_2010-04-23")
 
 carp_start_date <- as_date("2008-02-12")
 
@@ -109,6 +109,7 @@ carp_after_date_continual <- as_date("2017-08-10")
 
 # 3. useful functions for wrangling ---------------------------------------
 
+# create a column for "after" experimental removal
 after_dates_column <- function(df) {
   df %>% 
     mutate(after_dates = case_when(
@@ -155,7 +156,7 @@ exp_dates_column <- function(df) {
     exp_dates = fct_relevel(exp_dates, c("during", "after")))  
 }
 
-# make a new column for during and after and set factor levels
+# annual removal: make a new column for during and after and set factor levels
 exp_dates_column_annual <- function(df) {
   df %>% 
     mutate(exp_dates = case_when(
@@ -177,7 +178,7 @@ exp_dates_column_annual <- function(df) {
     exp_dates = fct_relevel(exp_dates, c("during", "after")))  
 }
 
-# make a new column for during and after and set factor levels
+# continual removal: make a new column for during and after and set factor levels
 exp_dates_column_continual <- function(df) {
   df %>% 
     mutate(exp_dates = case_when(
@@ -219,21 +220,35 @@ new_group_column <- function(df) {
       group == "invert" & taxon_family != "Pholadidae" & mobility == "sessile" ~ "epi_inverts",
       taxon_family == "Pholadidae" ~ "endo_inverts",
       sp_code %in% c("OPSP", "SPL", "LIGL", "SFL", "MECR") ~ "herb_inverts",
-      TRUE ~ "carn_inverts"
+      sp_code %in% c("COCA", "AML", "PGL", "PAIN", "PHL", "LOGR", "DIL", "KEKE", "PBL") ~ "carn_inverts"
     ))
 }
 
+# function to calculate standard error
+se <- function(x,...){
+  sd(x, na.rm = TRUE)/sqrt(length(na.omit(x)))
+}
 
 # 4. data -----------------------------------------------------------------
 
-############################################
-# a. LTE all species biomass #############
-############################################
+# ⟞ a. Max's guild data ---------------------------------------------------
+
+guilds <- read_csv(here::here("code/castorani", "LTE_guild_data.csv")) %>% 
+  mutate(sp.code = replace_na(sp.code, "Nandersoniana")) %>% 
+  rename("new_group" = biomass.guild)
+
+
+# ⟞ b. LTE all species biomass --------------------------------------------
 
 biomass <- read_csv(here::here("data", "LTE_All_Species_Biomass_at_transect_20220314.csv")) %>% 
   clean_names() %>% 
   # ANOB is incorrectly coded as having "SESSILE" mobility
   mutate(mobility = replace(mobility, sp_code == "ANOB", "MOBILE")) %>% 
+  # replace NA sp_code with Nandersoniana
+  mutate(sp_code = case_when(
+    scientific_name == "Nienburgia andersoniana" ~ "Nandersoniana",
+    TRUE ~ sp_code
+  )) %>% 
   # replace all -99999 values with NA
   mutate(dry_gm2 = replace(dry_gm2, dry_gm2 < 0, NA),
          wm_gm2 = replace(wm_gm2, wm_gm2 < 0, NA)) %>% 
@@ -247,8 +262,12 @@ biomass <- read_csv(here::here("data", "LTE_All_Species_Biomass_at_transect_2022
   exp_dates_column() %>% 
   # create a new column for season and set factor levels
   season_column() %>% 
+  # new group column %>% 
+  left_join(., guilds, by = c("sp_code" = "sp.code")) %>% 
   # take out all the first dates 
   filter(!(sample_ID %in% c(aque_start_dates, napl_start_dates, ivee_start_dates, mohk_start_dates, carp_start_dates)))
+
+# ⟞ c. LTE algae biomass at section ---------------------------------------
 
 biomass_section <- read_csv(here::here("data", "LTE_Algae_Biomass_at_section_20220422.csv")) %>% 
   clean_names() %>% 
@@ -266,9 +285,7 @@ biomass_section <- read_csv(here::here("data", "LTE_Algae_Biomass_at_section_202
   # take out all the first dates 
   filter(!(sample_ID %in% c(aque_start_dates, napl_start_dates, ivee_start_dates, mohk_start_dates, carp_start_dates)))
 
-############################################
-# b. percent cover
-############################################
+# ⟞ d. LTE percent cover --------------------------------------------------
 
 percov <- read_csv(here::here("data", "LTE_Cover_All_Years_20200605.csv")) %>% 
   clean_names() %>% 
@@ -283,17 +300,13 @@ percov <- read_csv(here::here("data", "LTE_Cover_All_Years_20200605.csv")) %>%
   # create a new column for season and set factor levels
   season_column()
 
-############################################
-# c. traits
-############################################
 
-traits <- read_csv(here::here("data/traits", "00-coarse_traits.csv"))
-
-############################################
-# d. LTER annual monitoring
-############################################
+# ⟞ e. annual benthics percent cover --------------------------------------
 
 percov_annual <- read_csv(here::here("data/benthics", "Annual_Cover_All_Years_20210108.csv"))
+
+
+# ⟞ f. annual benthics biomass --------------------------------------------
 
 biomass_annual <- read_csv(here::here("data/benthics", "Annual_All_Species_Biomass_at_transect_20210108.csv")) %>% 
   clean_names() %>% 
@@ -302,9 +315,7 @@ biomass_annual <- read_csv(here::here("data/benthics", "Annual_All_Species_Bioma
   # change to lower case
   mutate_at(c("group", "mobility", "growth_morph", "site"), str_to_lower)
 
-############################################
-# e. LTE kelp fronds ##############
-############################################
+# ⟞ g. LTE kelp fronds ----------------------------------------------------
 
 kelp_fronds <- read_csv(here::here("data", "LTE_Kelp_All_Years_20220202.csv")) %>% 
   clean_names() %>% 
@@ -326,12 +337,10 @@ kelp_fronds <- read_csv(here::here("data", "LTE_Kelp_All_Years_20220202.csv")) %
 # not in operator
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
-
 # 6. useful vectors and data frames ---------------------------------------
 
-############################################
-# a. species
-############################################
+
+# ⟞ a. species -----------------------------------------------------------
 
 # data frame of each species including scientific name, common name, and group
 spp_names <- biomass %>% 
@@ -392,9 +401,8 @@ common_algae <- c("PH", "PTCA", # Pterygophora californica
 # from rank abundance curves
 algae_selection <- c("PTCA", "DL", "CYOS", "CC", "R", "EC", "POLA", "RAT", "CO", "EGME", "BO", "AU")
 
-############################################
-# b. sites
-############################################
+
+# ⟞ b. sites --------------------------------------------------------------
 
 # vector of sites
 LTE_sites <- c("aque", "napl", "ivee", "mohk", "carp")
@@ -415,9 +423,7 @@ site_quality <- tribble(
 ) %>% 
   mutate(quality = fct_relevel(quality, c("low", "medium", "high")))
 
-############################################
-# c. sample IDs
-############################################
+# ⟞ c. sample IDs ---------------------------------------------------------
 
 # NAPL after
 napl_after_sampleIDs <- percov %>% 
@@ -435,7 +441,14 @@ all_after_sampleIDs <- percov %>%
   pull(sample_ID)
 
 
+# ⟞ d. today's date -------------------------------------------------------
+
+todays_date <- Sys.Date()
+
+
 # 7.  plot aesthetics -----------------------------------------------------
+
+# ⟞ a. site colors and shapes --------------------------------------------
 
 aque_col <- "#FDD989"
 carp_col <- "#516238"
@@ -443,14 +456,20 @@ ivee_col <- "#4CA2B0"
 mohk_col <- "#985E5C"
 napl_col <- "#D98A63"
 
-color_palette_site <- c("aque" = aque_col, "napl" = napl_col, "mohk" = mohk_col, "carp" = carp_col)
+color_palette_site <- c("aque" = aque_col, 
+                        "napl" = napl_col, 
+                        "mohk" = mohk_col, 
+                        "carp" = carp_col)
 
 aque_shape <- 21
 napl_shape <- 22
 mohk_shape <- 23
 carp_shape <- 24
 
-shape_palette_site <- c("aque" = aque_shape, "napl" = napl_shape, "mohk" = mohk_shape, "carp" = carp_shape)
+shape_palette_site <- c("aque" = aque_shape, 
+                        "napl" = napl_shape, 
+                        "mohk" = mohk_shape, 
+                        "carp" = carp_shape)
 
 annual_col <- "#54662C"
 continual_col <- "#009BB0"
@@ -458,13 +477,21 @@ control_col <- "grey"
 kelp_col <- "#6D5A18"
 under_col <- "#CC7540"
 
-color_palette <- c("annual" = annual_col, "continual" = continual_col, "control" = control_col)
+# ⟞ b. treatment colors and shapes ----------------------------------------
+
+color_palette <- c("annual" = annual_col, 
+                   "continual" = continual_col, 
+                   "control" = control_col)
 
 annual_shape <- 19
 continual_shape <- 17
 control_shape <- 21
 
-shape_palette <- c("annual" = annual_shape, "continual" = continual_shape, "control" = control_shape)
+shape_palette <- c("annual" = annual_shape, 
+                   "continual" = continual_shape, 
+                   "control" = control_shape)
+
+# ⟞ c. site full names ----------------------------------------------------
 
 aque_full <- "Arroyo Quemado (AQUE)"
 napl_full <- "Naples (NAPL)"
@@ -472,7 +499,11 @@ ivee_full <- "Isla Vista (IVEE)"
 mohk_full <- "Mohawk (MOHK)"
 carp_full <- "Carpinteria (CARP)"
 
-sites_full_levels <- c("Arroyo Quemado (AQUE)", "Naples (NAPL)", "Isla Vista (IVEE)", "Mohawk (MOHK)", "Carpinteria (CARP)")
+sites_full_levels <- c("Arroyo Quemado (AQUE)", 
+                       "Naples (NAPL)", 
+                       "Isla Vista (IVEE)", 
+                       "Mohawk (MOHK)", 
+                       "Carpinteria (CARP)")
 
 sites_full <- setNames(c("Arroyo Quemado (AQUE)",
                          "Naples (NAPL)",
@@ -494,12 +525,8 @@ sites_continual_full <- setNames(c("Arroyo Quemado (AQUE)",
                          "mohk",
                          "carp"))
 
-todays_date <- Sys.Date()
 
-# function to calculate standard error
-se <- function(x,...){
-  sd(x, na.rm = TRUE)/sqrt(length(na.omit(x)))
-}
+
 
 
 
