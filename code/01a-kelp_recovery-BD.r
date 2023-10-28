985ea6c50a87d3bee4afc24e29b5edb19f6750b6
an-bui <a.thanh.bui@gmail.com>
2023-10-28 11:17:32 -0700

    taking out the scaling

--- code/01a-kelp_recovery-BD.r
+++ code/01a-kelp_recovery-BD.r
@@ -18,7 +18,7 @@ summary(lm2)
 model_checks <- DHARMa::simulateResiduals(lm2) # pretty damn bad
 plot(model_checks)
 
-lm3 <- glmmTMB::glmmTMB(value ~ scale(time_since_end)*name + (1|site),
+lm3 <- glmmTMB::glmmTMB(value ~ time_since_end*name + (1|site),
                         data = df, family = ziGamma(link = "log"), ziformula = ~.) # So this is a Gamma hurdle model. I've included all the covariates in the zero-component. But you could play aroudn with this if you wanted. 
 summary(lm3)
 model_checks <- DHARMa::simulateResiduals(lm3) # pretty good! Not perfect but I feel like this is acceptable
@@ -76,15 +76,28 @@ df2 <- delta_continual %>%
   pivot_longer(cols = c(control, continual)) %>%
   filter(exp_dates == "after")
 
-lm5 <- glmmTMB::glmmTMB(value ~ scale(time_since_end)*name + (1|site),
+lm5 <- glmmTMB::glmmTMB(value ~ time_since_end*name + (1|site),
                         data = df2, family = ziGamma(link = "log"), ziformula = ~.) 
 summary(lm5)
-model_checks <- DHARMa::simulateResiduals(lm5) # pretty good1
+model_checks <- DHARMa::simulateResiduals(lm5) # pretty good?
 plot(model_checks)
 
-plot(ggpredict(lm5, terms = ~time_since_end*name), add.data = T)
+lm5_plot <- plot(ggpredict(lm5, terms = ~time_since_end*name), add.data = T)
 
+lm3_plot + lm5_plot
+delta_calc_during <- ggpredict(lm3, terms = ~time_since_end*name) %>% 
+  select(group, predicted, x) %>% 
+  pivot_wider(names_from = group, values_from = predicted) %>% 
+  mutate(delta_calc = continual - control)
 
+delta_calc_after <- ggpredict(lm5, terms = ~time_since_end*name) %>% 
+  select(group, predicted, x) %>% 
+  pivot_wider(names_from = group, values_from = predicted) %>% 
+  mutate(delta_calc = continual - control)
 
+ggplot(data = delta_continual, aes(x = time_since_end, y = delta_continual)) +
+  geom_point() +
+  geom_line(data = delta_calc_during, aes(x = x, y = delta_calc)) +
+  geom_line(data = delta_calc_after, aes(x = x, y = delta_calc))
 
 
