acb15e690a730d07343cf2a22dedfa982a84fd4c
an-bui <a.thanh.bui@gmail.com>
2023-12-06 09:20:02 -0800

    taking out old code

--- code/old-code/09-code_dump.R
+++ code/old-code/09-code_dump.R
@@ -1675,3 +1675,443 @@
 #              arrow = arrow(length = unit(0.5, "cm")),
 #              size = 1, lty = 2, color = carp_col) +
 # stat_ellipse(aes(group = comp_2yrs)) +
+
+##########################################################################-
+# 3. start-during-after comparisons ---------------------------------------
+##########################################################################-
+
+# ⟞ a. algae -------------------------------------------------------------
+
+# anova with random effect
+# anova_algae_1yr <- lmer(delta_continual_algae ~ comp_1yr + (1|site), 
+#                          data = delta_algae_continual %>% drop_na(comp_1yr))
+# anova_algae_2yrs <- lmer(delta_continual_algae ~ comp_2yrs + (1|site), 
+#                     data = delta_algae_continual %>% drop_na(comp_2yrs))
+# anova_algae_3yrs <- lmer(delta_continual_algae ~ comp_3yrs + (1|site), 
+#                          data = delta_algae_continual %>% drop_na(comp_3yrs))
+# 
+# anova_raw_algae_1yr <- lmer(algae_biomass ~ comp_1yr*treatment + (1|site),
+#                              data = algae_continual_long %>% drop_na(comp_1yr))
+# anova_raw_algae_2yrs <- lmer(algae_biomass ~ comp_2yrs*treatment + (1|site),
+#                              data = algae_continual_long %>% drop_na(comp_2yrs))
+# anova_raw_algae_3yrs <- lmer(algae_biomass ~ comp_3yrs*treatment + (1|site),
+#                              data = algae_continual_long %>% drop_na(comp_3yrs))
+
+# diagnostics
+# plot(simulateResiduals(anova_algae_1yr))
+# check_model(anova_algae_1yr)
+# 
+# plot(simulateResiduals(anova_algae_2yrs))
+# check_model(anova_algae_2yrs)
+# 
+# plot(simulateResiduals(anova_algae_3yrs))
+# check_model(anova_algae_3yrs)
+# 
+# plot(simulateResiduals(anova_raw_algae_1yr))
+# check_model(anova_raw_algae_1yr)
+# 
+# plot(simulateResiduals(anova_raw_algae_2yrs))
+# check_model(anova_raw_algae_2yrs)
+# 
+# plot(simulateResiduals(anova_raw_algae_3yrs))
+# check_model(anova_raw_algae_3yrs)
+
+# summary
+# summary(anova_algae_1yr) # difference when comparing start and after
+# summary(anova_algae_2yrs)
+# summary(anova_algae_3yrs) # same as 2 years
+# 
+# summary(anova_raw_algae_2yrs)
+# summary(anova_raw_algae_3yrs)
+
+# plot predictions
+# plot(ggpredict(anova_raw_algae_1yr, terms = c("comp_1yr", "treatment"), type = "fixed")) 
+# plot(ggpredict(anova_raw_algae_2yrs, terms = c("comp_2yrs", "treatment"), type = "fixed")) 
+# plot(ggpredict(anova_raw_algae_3yrs, terms = c("comp_3yrs", "treatment"), type = "fixed")) 
+
+# anova table
+# anova_algae_1yr_aovt <- anova(anova_algae_1yr, ddf = c("Kenward-Roger"))
+# 
+# anova_algae_2yrs_aovt <- anova(anova_algae_2yrs, ddf = c("Kenward-Roger"))
+# 
+# anova_algae_3yrs_aovt <- anova(anova_algae_3yrs, ddf = c("Kenward-Roger")) 
+# 
+# anova_algae_1yr_diff <- delta_algae_continual %>% 
+#   filter(comp_1yr %in% c("start", "during", "after")) %>% 
+#   group_by(comp_1yr) %>% 
+#   summarize(mean = mean(delta_continual_algae),
+#             se = se(delta_continual_algae))
+# 
+# anova_algae_2yrs_diff <- delta_algae_continual %>% 
+#   filter(comp_2yrs %in% c("start", "during", "after")) %>% 
+#   group_by(comp_2yrs) %>% 
+#   summarize(mean = mean(delta_continual_algae),
+#             se = se(delta_continual_algae))
+# 
+# anova_algae_3yrs_diff <- delta_algae_continual %>% 
+#   filter(comp_3yrs %in% c("start", "during", "after")) %>% 
+#   group_by(comp_3yrs) %>% 
+#   summarize(mean = mean(delta_continual_algae),
+#             se = se(delta_continual_algae))
+
+# least squares comparison
+
+# anova_algae_2yrs_summary <- difflsmeans_summary_fxn(anova_algae_2yrs)
+# 
+# anova_algae_3yrs_summary <- difflsmeans_summary_fxn(anova_algae_3yrs)
+
+# extract predicted values
+# anova_algae_2yrs_df <- ggpredict(anova_algae_2yrs, terms = "comp_2yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+# anova_algae_3yrs_df <- ggpredict(anova_algae_3yrs, terms = "comp_3yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+
+# ⟞ b. epilithic invertebrates -------------------------------------------
+
+# anova with random effect
+# anova_epi_1yr <- lmer(delta_continual_epi ~ comp_1yr + (1|site), 
+#                         data = delta_epi_continual %>% drop_na(comp_1yr))
+# anova_epi_2yrs <- lmer(delta_continual_epi ~ comp_2yrs + (1|site), 
+#                          data = delta_epi_continual %>% drop_na(comp_2yrs))
+# anova_epi_3yrs <- lmer(delta_continual_epi ~ comp_3yrs + (1|site), 
+#                          data = delta_epi_continual %>% drop_na(comp_3yrs))
+
+# diagnostics
+# plot(simulateResiduals(anova_epi_2yrs))
+# check_model(anova_epi_2yrs)
+# 
+# plot(simulateResiduals(anova_epi_3yrs))
+# check_model(anova_epi_3yrs)
+
+# summary
+# summary(anova_epi_2yrs)
+# summary(anova_epi_3yrs)
+
+# anova table
+# anova_epi_2yrs_aovt <- anova(anova_epi_2yrs, ddf = c("Kenward-Roger")) 
+# 
+# anova_epi_3yrs_aovt <- anova(anova_epi_3yrs, ddf = c("Kenward-Roger"))
+# 
+# anova_epi_2yrs_diff <- emmeans(anova_epi_2yrs, "comp_2yrs", lmer.df = "kenward-roger") %>% 
+#   test() %>% 
+#   as.data.frame() 
+# 
+# anova_epi_3yrs_diff <- emmeans(anova_epi_3yrs, "comp_3yrs", lmer.df = "kenward-roger") %>% 
+#   test() %>% 
+#   as.data.frame() 
+
+# least squares comparison
+# anova_epi_2yrs_summary <- difflsmeans_summary_fxn(anova_epi_2yrs)
+# 
+# anova_epi_3yrs_summary <- difflsmeans_summary_fxn(anova_epi_3yrs)
+
+# extract predicted values
+# anova_epi_2yrs_df <- ggpredict(anova_epi_2yrs, terms = "comp_2yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+# 
+# anova_epi_3yrs_df <- ggpredict(anova_epi_3yrs, terms = "comp_3yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+
+# ⟞ c. endolithic invertebrates ------------------------------------------
+
+# anova with random effect
+# anova_endo_2yrs <- lmer(delta_continual_endo ~ comp_2yrs + (1|site), 
+#                        data = delta_endo_continual %>% drop_na(comp_2yrs))
+# anova_endo_3yrs <- lmer(delta_continual_endo ~ comp_3yrs + (1|site), 
+#                        data = delta_endo_continual %>% drop_na(comp_3yrs))
+
+# diagnostics
+# plot(simulateResiduals(anova_endo_2yrs))
+# check_model(anova_endo_2yrs)
+# 
+# plot(simulateResiduals(anova_endo_3yrs))
+# check_model(anova_endo_3yrs)
+
+# summary
+# summary(anova_endo_2yrs)
+# summary(anova_endo_3yrs) # same as 2 years
+
+# anova table
+# anova_endo_2yrs_aovt <- anova(anova_endo_2yrs, ddf = c("Kenward-Roger"))
+# 
+# anova_endo_3yrs_aovt <- anova(anova_endo_3yrs, ddf = c("Kenward-Roger")) 
+# 
+# anova_endo_2yrs_diff <- emmeans(anova_endo_2yrs, "comp_2yrs", lmer.df = "kenward-roger") %>% 
+#   test() %>% 
+#   as.data.frame() 
+# 
+# anova_endo_3yrs_diff <- emmeans(anova_endo_3yrs, "comp_3yrs", lmer.df = "kenward-roger") %>% 
+#   test() %>% 
+#   as.data.frame() 
+
+# least squares comparison
+# anova_endo_2yrs_summary <- difflsmeans_summary_fxn(anova_endo_2yrs)
+# 
+# anova_endo_3yrs_summary <- difflsmeans_summary_fxn(anova_endo_3yrs)
+
+# extract predicted values
+# anova_endo_2yrs_df <- ggpredict(anova_endo_2yrs, terms = "comp_2yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+# anova_endo_3yrs_df <- ggpredict(anova_endo_3yrs, terms = "comp_3yrs", type = "fixed") %>% 
+#   mutate(x = case_when(
+#     x == "start" ~ "Start of removal",
+#     x == "during" ~ "End of removal",
+#     x == "after" ~ "Recovery period"
+#   )) %>% 
+#   mutate(x = fct_relevel(x, "Start of removal", "End of removal", "Recovery period"))
+
+# ⟞ d. figures -----------------------------------------------------------
+
+# ⟞ ⟞ i. algae -----------------------------------------------------------
+
+# sda_algae_biomass <- ggplot(anova_algae_2yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 125.5, ymax = 140, fill = "#FFFFFF") +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 2)) +
+#   # add in post-hoc comparisons
+#   annotate("text", x = 1, y = 122, label = "a", size = 3) +
+#   annotate("text", x = 2, y = 122, label = "b", size = 3) +
+#   annotate("text", x = 3, y = 122, label = "a", size = 3) +
+#   # annotate("text", x = 0.55, y = 135, label = "Understory algae", size = 10) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-30, 130), breaks = c(0, 50, 100, 150), expand = c(0, 0)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(a)")
+# sda_algae_biomass
+
+# sda_algae_biomass_3yrs <- ggplot(anova_algae_3yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 125.5, ymax = 140, fill = "#FFFFFF") +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 2)) +
+#   # add in post-hoc comparisons
+#   annotate("text", x = 1, y = 135, label = "a", size = 3) +
+#   annotate("text", x = 2, y = 135, label = "b", size = 3) +
+#   annotate("text", x = 3, y = 135, label = "a", size = 3) +
+#   # annotate("text", x = 0.55, y = 135, label = "Understory algae", size = 10) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-30, 140), breaks = c(0, 50, 100, 150), expand = c(0, 0)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(a) Understory macroalgae")
+# sda_algae_biomass_3yrs
+
+# ⟞ ⟞ ii. epi inverts ----------------------------------------------------
+
+# sda_epi_biomass <- ggplot(anova_epi_2yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 20.1, ymax = 24, fill = "#FFFFFF") +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 1)) +
+#   # add in post-hoc comparisons
+#   annotate("text", x = 1, y = 20, label = "a", size = 3) +
+#   annotate("text", x = 2, y = 20, label = "b", size = 3) +
+#   annotate("text", x = 3, y = 20, label = "b", size = 3) +
+#   # annotate("text", x = 0.68, y = 23, label = "Epilithic invertebrates", size = 10) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-13, 22), expand = c(0, 0)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(c)")
+# sda_epi_biomass
+
+# sda_epi_biomass_3yrs <- ggplot(anova_epi_3yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 20.1, ymax = 24, fill = "#FFFFFF") +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 1)) +
+#   # add in post-hoc comparisons
+#   annotate("text", x = 1, y = 20, label = "a", size = 3) +
+#   annotate("text", x = 2, y = 20, label = "b", size = 3) +
+#   annotate("text", x = 3, y = 20, label = "c", size = 3) +
+#   # annotate("text", x = 0.68, y = 23, label = "Epilithic invertebrates", size = 10) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-13, 22), expand = c(0, 0)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(b) Epilithic invertebrates")
+# sda_epi_biomass_3yrs
+
+# ⟞ ⟞ iii. endo inverts --------------------------------------------------
+
+# sda_endo_biomass <- ggplot(anova_endo_2yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 551, ymax = 650, fill = "#FFFFFF") +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 1)) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-100, 550), expand = c(0, 0), breaks = c(0, 200, 400, 600)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(e)")
+# sda_endo_biomass
+
+# sda_endo_biomass_3yrs <- ggplot(anova_endo_3yrs_df) +
+#   # horizontal line at 0
+#   geom_hline(yintercept = 0, lty = 2) +
+#   # annotate("rect", xmin = 0, xmax = 4, ymin = 551, ymax = 650, fill = "#FFFFFF") +
+#   # points and error bars
+#   geom_point(aes(x = x, y = predicted), size = 2) +
+#   geom_errorbar(aes(x = x, ymin = predicted - std.error, ymax = predicted + std.error), width = 0.2) +
+#   # path between time periods
+#   geom_line(aes(x = x, y = predicted, group = 1)) +
+#   # aesthetics
+#   scale_x_discrete(labels = wrap_format(10)) +
+#   scale_y_continuous(limits = c(-100, 550), expand = c(0, 0), breaks = c(0, 200, 400, 600)) +
+#   sda_biomass_theme() +
+#   labs(x = "Time period", 
+#        y = "\U0394 biomass \n (treatment - control)",
+#        title = "(c) Endolithic invertebrates")
+# sda_endo_biomass_3yrs
+
+##########################################################################-
+# 6. endo. invert linear model --------------------------------------------
+##########################################################################-
+
+# ⟞ a. during removal -----------------------------------------------------
+
+# ⟞ ⟞ i. model and diagnostics  -------------------------------------------
+
+# model
+lm_endo_during_lmer <- lmer(
+  delta_continual_endo ~ time_since_end + (1|site), 
+  data = delta_endo_continual %>% filter(exp_dates == "during"), 
+  na.action = na.pass
+)
+
+# check
+plot(simulateResiduals(lm_endo_during_lmer))
+check_model(lm_endo_during_lmer)
+
+# R2
+r.squaredGLMM(lm_endo_during_lmer)
+
+# summmary
+summary(lm_endo_during_lmer)
+lm_endo_during_summary <- lm_endo_during_lmer %>% 
+  tbl_regression() %>% 
+  bold_p(t = 0.05) %>% 
+  modify_header(
+    label = " ",
+    estimate = "**Slope**",
+    df = "**df**"
+  ) 
+lm_endo_during_summary
+
+# ⟞ ⟞ ii. predictions -----------------------------------------------------
+
+predicted_endo_during <- ggpredict(lm_endo_during_lmer, terms = ~ time_since_end, type = "fixed")
+
+# ⟞ b. recovery period ----------------------------------------------------
+
+# ⟞ ⟞ i. model and diagnostics  -------------------------------------------
+
+# model
+lm_endo_recovery_lmer <- lmer(
+  delta_continual_endo ~ time_since_end + (1|site), 
+  data = delta_endo_continual %>% filter(exp_dates == "after"), 
+  na.action = na.pass
+)
+
+# check
+plot(simulateResiduals(lm_endo_recovery_lmer))
+check_model(lm_endo_recovery_lmer)
+
+# R2
+r.squaredGLMM(lm_endo_recovery_lmer)
+
+# summary
+summary(lm_endo_recovery_lmer)
+lm_endo_recovery_summary <- lm_endo_recovery_lmer %>% 
+  tbl_regression() %>% 
+  bold_p(t = 0.05) %>% 
+  modify_header(
+    label = " ",
+    estimate = "**Slope**",
+    df = "**df**"
+  ) 
+lm_endo_recovery_summary
+
+# ⟞ ⟞ ii. predictions -----------------------------------------------------
+
+predicted_endo_recovery <- ggpredict(lm_endo_recovery_lmer, terms = ~ time_since_end, type = "fixed")
+
+# ⟞ c. figure ------------------------------------------------------------
+
+endo_time <- ggplot() +
+  geom_vline(xintercept = 0, lty = 2) +
+  geom_hline(yintercept = 0, lty = 2) +
+  geom_point(data = delta_endo_continual, 
+             aes(x = time_since_end, y = delta_continual_endo, fill = site, shape = site), size = 2, alpha = 0.9) +
+  scale_shape_manual(values = shape_palette_site) +
+  scale_fill_manual(values = color_palette_site) +
+  # new_scale("color") + 
+  # overall
+  # geom_line(data = predicted_clam_after, aes(x = x, y = predicted), size = 2, alpha = 0.7) +
+  # geom_ribbon(data = predicted_clam_after, aes(x = x, ymax = conf.high, ymin = conf.low), alpha = 0.2) +
+  # geom_line(data = predicted_clam_during, aes(x = x, y = predicted), size = 2, alpha = 0.7) +
+  # geom_ribbon(data = predicted_clam_during, aes(x = x, ymax = conf.high, ymin = conf.low), alpha = 0.2) +
+  scale_x_continuous(breaks = seq(-8, 6, by = 1), minor_breaks = NULL) +
+  # scale_y_continuous(breaks = seq(-250, 750, by = 250), limits = c(-250, 750)) +
+  delta_timeseries_theme("endo") +
+  labs(x = "Time since end of removal (years)", 
+       y = "\U0394 biomass \n (treatment - control)",
+       subtitle = "(f)")
+endo_time
